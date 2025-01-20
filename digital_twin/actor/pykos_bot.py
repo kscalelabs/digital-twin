120f0dcb14d283ea6410e7efc78f6e8670d792ee
Wesley Maa <wesley.maa@gmail.com>
2025-01-20 13:30:12 -0800

    pykos actor

--- digital_twin/actor/pykos_bot.py
+++ digital_twin/actor/pykos_bot.py
@@ -1,5 +1,7 @@
 """Defines an actor robot model that communicates with a robot using PyKOS."""
 
+import math
+
 from pykos import KOS
 
 from digital_twin.actor.base import ActorRobot
@@ -12,7 +14,17 @@ class PyKOSActor(ActorRobot):
         self,
         kos: KOS,
         joint_names: dict[str, int],
+        kos_offsets: dict[str, float] | None = None,
+        kos_signs: dict[str, float] | None = None,
     ) -> None:
+        """Initialize the PyKOS actor.
+
+        Args:
+            kos: The PyKOS instance to use.
+            joint_names: A dictionary mapping joint names to their IDs.
+            kos_offsets: A dictionary mapping joint names to their offsets in degrees.
+            kos_signs: A dictionary mapping joint names to their signs.
+        """
         self.kos = kos
         self.joint_names_to_ids = joint_names
         self.joint_ids_to_names = {v: k for k, v in joint_names.items()}
@@ -20,7 +32,29 @@ class PyKOSActor(ActorRobot):
             raise ValueError("Joint IDs must be unique")
         self.joint_ids = list(sorted(list(joint_names.values())))
 
-    async def get_joint_angles(self) -> dict[str, float]:
+        self.current_offsets = kos_offsets
+        self.current_signs = kos_signs
+        if kos_signs is None:
+            self.current_signs = {k: 1 for k in self.joint_names_to_ids}
+
+    def get_raw_angles(self) -> dict[int, float]:
         states = self.kos.actuator.get_actuators_state(self.joint_ids)
-        id_to_angle = {state.actuator_id: state.position for state in states.states}
-        return {self.joint_ids_to_names[id]: angle for id, angle in id_to_angle.items()}
+        return {state.actuator_id: state.position for state in states.states}
+
+    def get_named_angles(self, radians: bool = True) -> dict[str, float]:
+        return {self.joint_ids_to_names[id]: math.radians(angle) if radians else angle for id, angle in self.get_raw_angles().items()}
+
+    def offset_in_place(self) -> None:
+        self.current_offsets = {k: v * -1 for k, v in self.get_named_angles(radians=False).items()}
+
+    async def get_joint_angles(self) -> dict[str, float]:
+        if self.current_offsets is None:
+            print("No offsets set, returning values directly")
+            return self.get_named_angles(radians=True)
+        return {
+            name: math.radians((angle + self.current_offsets[name]) * self.current_signs[name])
+            for name, angle in self.get_named_angles(radians=False).items()
+        }
+
+    def get_offsets(self) -> dict[str, float] | None:
+        return self.current_offsets
--- examples/pykos_test.py
+++ examples/pykos_test.py
@@ -1,14 +1,82 @@
 """Test PyKOS communication."""
 
+import argparse
+import asyncio
+
 from pykos import KOS
 
 from digital_twin.actor.pykos_bot import PyKOSActor
+from digital_twin.puppet.mujoco_puppet import MujocoPuppet
+
+joint_mapping = {
+    # Left arm
+    "left_shoulder_yaw": 11,
+    "left_shoulder_pitch": 12,
+    "left_elbow_yaw": 13,
+    "left_gripper": 14,
+    # Right arm
+    "right_shoulder_yaw": 21,
+    "right_shoulder_pitch": 22,
+    "right_elbow_yaw": 23,
+    "right_gripper": 24,
+    # Left leg
+    "left_hip_yaw": 31,
+    "left_hip_roll": 32,
+    "left_hip_pitch": 33,
+    "left_knee_pitch": 34,
+    "left_ankle_pitch": 35,
+    # Right leg
+    "right_hip_yaw": 41,
+    "right_hip_roll": 42,
+    "right_hip_pitch": 43,
+    "right_knee_pitch": 44,
+    "right_ankle_pitch": 45
+}
+
+signs: dict[str, float] | None = {
+    # Left arm
+    "left_shoulder_yaw": 1,
+    "left_shoulder_pitch": -1,
+    "left_elbow_yaw": -1,
+    "left_gripper": 1,
+    # Right arm
+    "right_shoulder_yaw": 1,
+    "right_shoulder_pitch": 1,
+    "right_elbow_yaw": 1,
+    "right_gripper": 1,
+    # Left leg
+    "left_hip_yaw": 1,
+    "left_hip_roll": -1,
+    "left_hip_pitch": 1,
+    "left_knee_pitch": 1,
+    "left_ankle_pitch": 1,
+    # Right leg
+    "right_hip_yaw": 1,
+    "right_hip_roll": 1,
+    "right_hip_pitch": 1,
+    "right_knee_pitch": 1,
+    "right_ankle_pitch": 1
+}
 
 
-def main() -> None:
-    kos = KOS()
-    _actor = PyKOSActor(kos, {"shoulder": 0, "elbow": 1, "wrist": 2})
+async def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mjcf_name", type=str, help="Name of the Mujoco model in the K-Scale API")
+    parser.add_argument("--ip", type=str, default="localhost", help="IP address of the robot")
+    args = parser.parse_args()
+
+    kos = KOS(ip=args.ip)
+    actor = PyKOSActor(kos, joint_mapping, kos_signs=signs)
+    actor.offset_in_place()
+    print(f"Offsets: {actor.get_offsets()}")
+    puppet = MujocoPuppet(args.mjcf_name)
+
+    while True:
+        joint_angles = await actor.get_joint_angles()
+        print(joint_angles)
+        await puppet.set_joint_angles(joint_angles)
+        await asyncio.sleep(0.01)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
