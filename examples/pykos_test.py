af16a607dfd0f9a5d716e9682b27ce8ba8da67ee
Wesley Maa <wesley.maa@gmail.com>
2025-02-06 06:32:21 -0800

    update names

--- examples/pykos_test.py
+++ examples/pykos_test.py
@@ -21,58 +21,58 @@ class KbotConfigs(RobotConfigs):
     joint_mapping: dict[str, int] = field(
         default_factory=lambda: {
             # Left arm
-            "L_shoulder_y_03": 11,
-            "L_shoulder_x_03": 12,
-            "L_shoulder_z_02": 13,
-            "L_elbow_02": 14,
-            "L_wrist_02": 15,
+            "left_shoulder_pitch_03": 11,
+            "left_shoulder_roll_03": 12,
+            "left_shoulder_yaw_02": 13,
+            "left_elbow_02": 14,
+            "left_wrist_02": 15,
             # Right arm
-            "R_shoulder_y_03": 21,
-            "R_shoulder_x_03": 22,
-            "R_shoulder_z_02": 23,
-            "R_elbow_02": 24,
-            "R_wrist_02": 25,
+            "right_shoulder_pitch_03": 21,
+            "right_shoulder_roll_03": 22,
+            "right_shoulder_yaw_02": 23,
+            "right_elbow_02": 24,
+            "right_wrist_02": 25,
             # Left leg
-            "L_hip_y_04": 31,
-            "L_hip_x_03": 32,
-            "L_hip_z_03": 33,
-            "L_knee_04": 34,
-            "L_ankle_02": 35,
+            "left_hip_pitch_04": 31,
+            "left_hip_roll_03": 32,
+            "left_hip_yaw_03": 33,
+            "left_knee_04": 34,
+            "left_ankle_02": 35,
             # Right leg
-            "R_hip_y_04": 41,
-            "R_hip_x_03": 42,
-            "R_hip_z_03": 43,
-            "R_knee_04": 44,
-            "R_ankle_02": 45,
+            "right_hip_pitch_04": 41,
+            "right_hip_roll_03": 42,
+            "right_hip_yaw_03": 43,
+            "right_knee_04": 44,
+            "right_ankle_02": 45,
         }
     )
 
     signs: dict[str, int] = field(
         default_factory=lambda: {
             # Left arm
-            "L_shoulder_y_03": 1,
-            "L_shoulder_x_03": 1,
-            "L_shoulder_z_02": 1,
-            "L_elbow_02": 1,
-            "L_wrist_02": 1,
+            "left_shoulder_pitch_03": 1,
+            "left_shoulder_roll_03": 1,
+            "left_shoulder_yaw_02": 1,
+            "left_elbow_02": -1,
+            "left_wrist_02": 1,
             # Right arm
-            "R_shoulder_y_03": 1,
-            "R_shoulder_x_03": 1,
-            "R_shoulder_z_02": 1,
-            "R_elbow_02": 1,
-            "R_wrist_02": 1,
+            "right_shoulder_pitch_03": 1,
+            "right_shoulder_roll_03": 1,
+            "right_shoulder_yaw_02": 1,
+            "right_elbow_02": -1,
+            "right_wrist_02": 1,
             # Left leg
-            "L_hip_y_04": 1,
-            "L_hip_x_03": 1,
-            "L_hip_z_03": 1,
-            "L_knee_04": -1,
-            "L_ankle_02": 1,
+            "left_hip_pitch_04": 1,
+            "left_hip_roll_03": 1,
+            "left_hip_yaw_03": 1,
+            "left_knee_04": 1,
+            "left_ankle_02": 1,
             # Right leg
-            "R_hip_y_04": 1,
-            "R_hip_x_03": 1,
-            "R_hip_z_03": 1,
-            "R_knee_04": 1,
-            "R_ankle_02": 1,
+            "right_hip_pitch_04": 1,
+            "right_hip_roll_03": 1,
+            "right_hip_yaw_03": 1,
+            "right_knee_04": 1,
+            "right_ankle_02": -1,
         }
     )
 
